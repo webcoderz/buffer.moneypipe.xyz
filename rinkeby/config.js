79826f90d2a6e7e0029b94d92c6dfe192f051a79
skogard <skogard@protonmail.com>
2021-12-29 14:15:23 -0500

    init

---
+++ rinkeby/config.js
@@ -0,0 +1,8 @@
+const Config = {
+//  network: "rinkeby",
+//  address: "0x222410467D455fE1029c434bf5AC86A0F711880E"
+//  address: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
+//  address: "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9"
+  network: "rinkeby",
+  address: "0x88562602bB851c871A085f3E512aCfcea27b7fd4"
+}
