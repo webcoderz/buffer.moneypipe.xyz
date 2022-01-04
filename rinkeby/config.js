52d1e075d07a152385864cf570c1c876d3079a2c
skogard <skogard@protonmail.com>
2022-01-04 09:00:52 -0500

    use moneypipe.js

--- rinkeby/config.js
+++ rinkeby/config.js
@@ -5,5 +5,6 @@ const Config = {
 //  address: "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9"
   network: "rinkeby",
 //  address: "0x88562602bB851c871A085f3E512aCfcea27b7fd4"
-  address: "0xE58b009F9De02Cd3638f4d38F94C953875F46b75"
+//  address: "0xE58b009F9De02Cd3638f4d38F94C953875F46b75"
+  address: "0x6706c0452C82f1d57d25860A129627210438c075"
 }
